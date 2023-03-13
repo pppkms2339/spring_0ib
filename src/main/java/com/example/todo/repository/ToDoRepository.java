e519644eeb67a9a2d1bf33628e0b61818bad53e6
Anton Loshmanov <pppkms2339@gmail.com>
2023-03-14 09:48:19 +1000

    Spring Data JDBC (начало)

--- src/main/java/com/example/todo/repository/ToDoRepository.java
+++ src/main/java/com/example/todo/repository/ToDoRepository.java
@@ -1,29 +1,57 @@
 package com.example.todo.repository;
 
 import com.example.todo.entity.ToDo;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
 import java.time.LocalDateTime;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 @Repository
-public class ToDoRepository implements CommonRepository<ToDo>{
+public class ToDoRepository implements CommonRepository<ToDo> {
 
-    private Map<String, ToDo> toDos = new HashMap<>();
+    public static final String SQL_UPDATE = "UPDATE todo SET description = :description, created = :created, updated = :updated, completed = :completed WHERE id = :id";
+    public static final String SQL_INSERT = "INSERT INTO todo (id, description, created, updated, completed) VALUES (:id, :description, :created, :updated, :completed)";
+
+    public static final String SQL_SELECT_ALL = "SELECT * FROM todo";
+
+    public static final String SQL_SELECT_BY_ID = "SELECT * FROM todo WHERE id = :id";
+
+    public static final String SQL_DELETE = "DELETE FROM todo WHERE id = :id";
+
+    private final NamedParameterJdbcTemplate jdbcTemplate;
+
+    public ToDoRepository(NamedParameterJdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    private RowMapper<ToDo> toDoRowMapper = (ResultSet rs, int rowNum) -> {
+        ToDo toDo = new ToDo();
+        toDo.setId(rs.getString("id"));
+        toDo.setDescription(rs.getString("description"));
+        toDo.setCreated(rs.getTimestamp("created").toLocalDateTime());
+        toDo.setUpdated(rs.getTimestamp("updated").toLocalDateTime());
+        toDo.setCompleted(rs.getBoolean("completed"));
+        return toDo;
+    };
 
     @Override
     public ToDo save(ToDo entity) {
-        ToDo result = toDos.get(entity.getId());
+        ToDo result = findById(entity.getId());
         if (result != null) {
-            result.setUpdated(LocalDateTime.now());
+            // Обновление записи
             result.setDescription(entity.getDescription());
             result.setCompleted(entity.isCompleted());
-            entity = result;
+            result.setUpdated(LocalDateTime.now());
+            return upsert(result, SQL_UPDATE);
         }
-        toDos.put(entity.getId(), entity);
-        return toDos.get(entity.getId());
+        return upsert(entity, SQL_INSERT);
     }
 
     @Override
@@ -34,16 +62,32 @@ public class ToDoRepository implements CommonRepository<ToDo>{
 
     @Override
     public void delete(ToDo entity) {
-        toDos.remove(entity.getId());
+        jdbcTemplate.update(SQL_DELETE, Collections.singletonMap("id", entity.getId()));
     }
 
     @Override
     public ToDo findById(String id) {
-        return toDos.get(id);
+        try {
+            return jdbcTemplate.queryForObject(SQL_SELECT_BY_ID, Collections.singletonMap("id", id), toDoRowMapper);
+        } catch (EmptyResultDataAccessException e) {
+            return null;
+        }
     }
 
     @Override
     public Collection<ToDo> findAll() {
-        return toDos.values();
+        return jdbcTemplate.query(SQL_SELECT_ALL, toDoRowMapper);
     }
+
+    private ToDo upsert(ToDo toDo, String sql) {
+        Map<String, Object> namedParameters = new HashMap<>();
+        namedParameters.put("id", toDo.getId());
+        namedParameters.put("description", toDo.getDescription());
+        namedParameters.put("created", toDo.getCreated());
+        namedParameters.put("update", toDo.getUpdated());
+        namedParameters.put("completed", toDo.isCompleted());
+        jdbcTemplate.update(sql, namedParameters);
+        return findById(toDo.getId());
+    }
+
 }
